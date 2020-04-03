c43c412b291262564268b8d11cddaff90b0449c5
Kennard Elliott <kennardelliott@Kennards-MacBook-Air.local>
2020-04-03 14:06:23 -0500

    Final changes before heroku deployment

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:kennard@localhost/project1'
+#app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://irgzkebawokoll:4fa46aa6e90ecef44c58f725181bffb7d00f69e40b930fc49578099df28c9ed7@ec2-18-215-99-63.compute-1.amazonaws.com:5432/daspicoks1kchk'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
@@ -14,4 +15,5 @@ migrate = Migrate(app, db)
 
 
 app.config.from_object(__name__)
+
 from app import views
